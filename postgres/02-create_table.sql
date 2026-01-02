d90e19dcb3170dd973ec26ae4850b278e8cb7435
ncut-d1z <dong1z@mail.ncut.edu.cn>
2026-01-02 23:17:03 +0800

    Update

--- postgres/02-create_table.sql
+++ postgres/02-create_table.sql
@@ -10,6 +10,5 @@ CREATE TABLE raw_traffic_data (
 );
 
 -- 导入 CSV 数据
-COPY raw_traffic_data(road_seg_id, data_time, volume, speed)
-FROM PROGRAM 'sh -c "cat /app/data/*.csv"'
-WITH ( FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8' );
+-- awk 'NR==1 || FNR>1' data-old/*.csv > data/final.csv
+-- grep -v "road_seg_id" final.csv | su - postgres -c "psql -U postgres -d traffic_db -c '\copy raw_traffic_data(road_seg_id, data_time, volume, speed) FROM STDIN WITH (FORMAT csv, HEADER false)'"
